5a780e835b08409470045dd675258376f0073068
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2025-01-22 16:19:11 +0100

    add unit test

--- src/test/java/com/example/springbootstarter/SpringBootStarterApplicationTests.java
+++ src/test/java/com/example/springbootstarter/SpringBootStarterApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.springbootstarter;
 
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.springbootstarter.Controllers.UserController;
+
 import org.junit.jupiter.api.Test;
+import org.modelmapper.internal.util.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class SpringBootStarterApplicationTests {
 
+    @Autowired
+    private UserController _userController;
+
     @Test
     void contextLoads() {
+        assert _userController != null;
     }
 
+    @Test
+    void Hello_ReturnsHello(){
+        var result = _userController.hello();
+
+        Assert.notNull(result);
+        assertEquals("Hello World!", result.getBody());
+        assertInstanceOf(ResponseEntity.class, result);
+    }
 }
